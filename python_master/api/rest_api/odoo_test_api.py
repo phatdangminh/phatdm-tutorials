f5ced5bd46c40bc35a0e063de9c566861eddd3a9
Phat Dang Minh <phatdm@bap-ventures.com>
2021-11-29 13:49:56 +0700

    Update

--- python_master/api/rest_api/odoo_test_api.py
+++ python_master/api/rest_api/odoo_test_api.py
@@ -6,7 +6,7 @@ import requests
 
 # Base URL: Default for Dev Server BEMO
 base_url = 'https://dev-bap.bemo.link'
-
+base_localhost_url = 'http://localhost:8071'
 
 # ==========================
 # Rest API - Base Methods
@@ -17,27 +17,55 @@ base_url = 'https://dev-bap.bemo.link'
 #     return requests.get(url, headers, data)
 
 
+r = requests.get(
+    url=base_localhost_url + '/api/auth/get_tokens',
+    headers={'Content-Type': 'text/html; charset=utf-8'},
+    data=json.dumps({
+        'username': 'admin@bap.bemo.cloud1',
+        'password': '1',
+    }),
+)
+access_token = r.json()['access_token']
+print(access_token)
+
+# ==========================
+# Rest API - Test Case
+# ==========================
+
+model_object = 'hr.employee'
+
+get_all = requests.get(
+    url=base_localhost_url + '/api/%s?limit=20&order=id' % model_object,
+    headers={
+        'Content-Type': 'text/html; charset=utf-8',
+        'Access-Token': access_token,
+    },
+)
+
+pprint.pprint(get_all.json(), indent=4)
+# print("Res = %s" % get_all.json())
+
 # ==========================
 # Rest API - Model [hr.job]
 # ==========================
 
-hr_job = 'hr.job'
+# hr_job = 'hr.job'
 
 # {API - GET}: List Jobs
-get_list_jobs = requests.get(
-    url=base_url + '/api/v1/%s' % hr_job,
-    headers={
-        'Content-Type': 'text/html; charset=utf-8',
-        'Bap-Access-Token': '3620a418446d6b36551c1db4700d754b'
-    },
-    data={}
-)
-
-pprint.pprint("Status: {},\nCount: {},\nResult: {}".format(
-    get_list_jobs.status_code, 
-    get_list_jobs.json()['count'], 
-    get_list_jobs.json()['results'], 
-    indent=4))
+# get_list_jobs = requests.get(
+#     url=base_url + '/api/v1/%s' % hr_job,
+#     headers={
+#         'Content-Type': 'text/html; charset=utf-8',
+#         'Bap-Access-Token': '3620a418446d6b36551c1db4700d754b'
+#     },
+#     data={}
+# )
+#
+# pprint.pprint("Status: {},\nCount: {},\nResult: {}".format(
+#     get_list_jobs.status_code,
+#     get_list_jobs.json()['count'],
+#     get_list_jobs.json()['results'],
+#     indent=4))
 
 # {API - POST}: Create Job
 
