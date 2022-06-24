f0cdb26e4770f6be2f4d2c379705083e5ba14bc7
PhatDM <phatdm@bap-ventures.com>
2022-06-24 11:27:38 +0700

    Update

--- python_master/tests/test_case.py
+++ python_master/tests/test_case.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+from googletrans import Translator
+import pprint
 
-print('============= TEST CASEs =============')
+
+print('============= TEST =============')
+
+translator = Translator()
+res = translator.translate('Mitä sinä teet', src='vi')
+
+# pprint.pprint(googletrans.LANGUAGES, indent=4, sort_dicts=True)
+print(res.src)
+print(res.dest)
+print(res.origin)
+print(res.text)
+print(res.pronunciation)
